16d7d5030ce9b8861812310ab6425199e16e042b
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-22 12:45:44 -0300

    computed property total perPerson

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,6 +14,17 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0] //percentages picker
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationStack{
             Form {
@@ -36,8 +47,8 @@ struct ContentView: View {
                     }
                     .pickerStyle(.segmented)
                 }
-                Section{
-                    Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section("Total per person (with tip included)") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
