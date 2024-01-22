e967465535ca4fcea788e4fea37f8070cc60da9a
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-22 12:09:16 -0300

    amount text field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,22 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        NavigationStack {
-            Form {
-                TextField("Enter your name", text: $name)
-                Text("Your name is \(name.uppercased())")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
