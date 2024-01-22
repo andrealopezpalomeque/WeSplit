c3da9b0e717cabad749778ac66c73d75f9e7ce15
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-22 11:38:05 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrea Victoria López Palomeque on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
